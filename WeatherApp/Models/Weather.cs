fe76604f7d05d3169958c15662d38bf959fe969b
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-11-03 12:54:46 -0400

    first commit

---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    class Weather
+    {
+        public Object getWeatherForecast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=17144ac78ad8f6aef3c2ba3d77e8ff51&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
